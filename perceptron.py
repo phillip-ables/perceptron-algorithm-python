15de6cebc3073cc7d00faff0256a14e301441140
Phillip <phillipables1@gmail.com>
2018-02-18 14:58:03 -0500

    for _ in range method to finish off our fit method and it returns the number of errors in the program

--- perceptron.py
+++ perceptron.py
@@ -24,4 +24,35 @@ class Perceptron(object):
     errors_ : list
         Number of misclassification in every epoch.
     ""
-    def __init__(self, eta=0.01, n_iter=10)
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, x, y):
+        ""
+        Fit method for training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+            Training vectors, where `n_samples   is the Number
+            of samples and `n_features` is the number of
+            featuresself.
+        y : {array-like}, shape = [n_samples]
+            Target values
+        Returns
+        -------
+        self : object
+        ""
+        self.w_= np.zeros(1+ X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
